803740d2f4eca70b2efd89c11ca40384fc4ed25b
Dragon18 <57226202+dragon182118@users.noreply.github.com>
2024-06-20 19:40:01 -0300

    bug fix parte 5, create button on mobile first and more

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 window.onload = function () {
     // Declarações globais
     let alavanca = 0;
+    let ala = 2;
     const dDia = document.querySelector('#dia');
     const dMes = document.querySelector('#mes');
     const dAno = document.querySelector('#ano');
     let year = new Date().getFullYear();
     let month = new Date().getMonth();
-
+    const tbody = document.getElementById("tCorpo");
+    var newNome = {};
     // Registro dos dias
     const diasdoMes = () => {
 
@@ -19,23 +21,23 @@ window.onload = function () {
     }
 
     // Ligação com o click da checkbox
-    document.getElementById("cbox").addEventListener("click", () => {
-        alavanca++
-        if (alavanca <= 1) {
-            criarTabela(1);
-        }
-        alavanca = 2;
-        dDia.textContent = 2;
-    });
-
+    if (alavanca == 0) {
+        document.getElementById('table').addEventListener("click", () => {
+            alavanca++
+            if (alavanca <= 1) {
+                criarTabela(1, 2);
+                dDia.textContent = 1;
+                document.getElementById('inicializador').remove();
+            }
+            alavanca = 2;
+        });
+    }
 
     // Cria o novo elemento na tabela
-    const criarTabela = (contador) => {
+    const criarTabela = (contador, tdTabelaX) => {
         for (let i = 0; i < contador; i++) {
 
-            const tbody = document.getElementById("tCorpo");
-
-            const exEl = document.getElementById(i + 2)
+            const exEl = document.getElementById(i + 1)
 
             if (exEl) {
                 exEl.parentElement.parentElement.remove();
@@ -45,27 +47,20 @@ window.onload = function () {
 
             let tdTabela1 = document.createElement("td");
             let tdTabela2 = document.createElement("td");
-            let tdTabela3 = document.createElement("td");
-            let tdTabela4 = document.createElement("td");
 
             let labelTd = document.createElement("label");
-
             let inputTd = document.createElement("input");
 
             tdTabela1.className = "cent";
             labelTd.className = "number";
-            labelTd.setAttribute("for", `cbox${i + 2}`)
-            labelTd.textContent = i + 2;
-            labelTd.id = i + 2;
+            labelTd.setAttribute("for", `cbox${i + 1}`)
+            labelTd.textContent = i + 1;
+            labelTd.id = i + 1;
 
             tdTabela2.className = "cent";
             inputTd.type = "checkbox";
-            inputTd.name = `cbox${i + 2}`;
-            inputTd.id = `cbox${i + 2}`;
-
-            tdTabela3.textContent = "Flexão";
-
-            tdTabela4.textContent = "Prancha";
+            inputTd.name = `cbox${i + 1}`;
+            inputTd.id = `cbox${i + 1}`;
 
             tdTabela1.appendChild(labelTd);
 
@@ -73,18 +68,22 @@ window.onload = function () {
 
             trTabela.appendChild(tdTabela1);
             trTabela.appendChild(tdTabela2);
-            trTabela.appendChild(tdTabela3);
-            trTabela.appendChild(tdTabela4);
+
+            for (let z = 0; z < tdTabelaX; z++) {
+
+                let tdTabela0 = {};
+                tdTabela0[z] = document.createElement("td");
+                tdTabela0[z].textContent = Exnomes(z);
+                trTabela.appendChild(tdTabela0[z]);
+            }
 
             tbody.appendChild(trTabela);
 
         }
         dDia.textContent++;
 
-        let contador2 = contador + 1;
-
-        if (!(dDia.textContent == diasdoMes())) {
-            trConstructor(contador2);
+        if (!(contador == diasdoMes())) {
+            trConstructor(contador);
         } else {
             // Mudanças dos dias 
             dMes.textContent++;
@@ -93,30 +92,45 @@ window.onload = function () {
                 dAno.textContent++; dMes.textContent = ''
             };
         }
-        checks(contador2);
+        checks(contador);
     }
     const callbacks = {};
 
+    document.getElementById('exNew').addEventListener('click', () => {
+        ala ++;
+        newNome = prompt('Digite o nome do novo exercício');
+        // funcionando, pegar os ids para modificar a tabela de acordo com o clique desse botão... th(thead). ver o que fazer com o tfoot...
+        
+    })
+    // Nomes dinamicos
+    function Exnomes(z) {
+        let exnome = ['Flexão', 'Prancha'];
+        if (newNome[z]) {
+            exnome.push(newNome);
+        }
+        return exnome[z];
+    }
     function trConstructor(cc) {
         const input = document.getElementById(`cbox${cc}`);
         const callback = () => {
-            criarTabela(cc);
+            criarTabela(cc + 1, ala);
         };
 
         callbacks[cc] = callback;
         input.addEventListener('click', callback);
 
-        for (let y = 2; y < cc; y++) {
+        for (let y = 1; y < cc; y++) {
             let input2 = document.getElementById(`cbox${y}`);
             if (input2 && callbacks[y]) {
                 input2.removeEventListener('click', callbacks[y]);
             }
         }
     }
+
     // check ativo / reset da tabela
     const checks = (cc) => {
         //cheks
-        for (let x = 2; x < cc; x++) {
+        for (let x = 1; x < cc; x++) {
             let input2 = document.getElementById(`cbox${x}`);
             if (input2 && callbacks[x]) {
                 input2.checked = true;
@@ -127,13 +141,20 @@ window.onload = function () {
             if (callbacks[cc - 1]) {
                 let input3 = document.getElementById(`cbox${cc}`);
                 input3.addEventListener('click', () => {
-                    const tbody = document.getElementById("tCorpo");
+
                     if (tbody.children.length > 0) {
-                        let firstTR = document.getElementById('tr1');
-                        tbody.innerHTML = firstTR.outerHTML;
+                        let firstTR = tbody.children[0].innerHTML;
+                        tbody.innerHTML = firstTR;
                     }
                     alavanca = 0;
                     dDia.textContent = 1;
+                    //reatribuição de eventlistener
+                    for (let i = 1; i <= diasdoMes(); i++) {
+                        let input2 = document.getElementById(`cbox${i}`);
+                        if (input2 && callbacks[i]) {
+                            input2.addEventListener('click', callbacks[i]);
+                        }
+                    }
                 });
             }
         }
